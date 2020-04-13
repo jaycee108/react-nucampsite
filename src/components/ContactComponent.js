51a6415b00801976a255e862861b45ab06c1e670
Jennifer Casal <jennifercasal@gmail.com>
2020-04-12 21:05:10 -0700

    Week 5 Workshop

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, Form, Errors, actions } from 'react-redux-form';
+import { Control, Form, Errors } from 'react-redux-form';
+import { postFeedback } from '../redux/ActionCreators';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -34,9 +35,7 @@ class Contact extends Component {
 
     
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
-        this.props.resetFeedbackForm();
+        postFeedback(values.firstName, values.lastName, values.phoneNum, values.email, values.agree, values.contactType, values.message, values.message, values.feedback, values.id);
     }
 
     render() {
